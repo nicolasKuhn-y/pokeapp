96d816aaa1283d5cc5e896769f8c6cf20031fca0
nicolas <nicolaskuhn02@gmail.com>
2020-12-15 12:28:16 -0300

    Refactorizacion y modularizacion

---
+++ src/Scripts/Card.js
@@ -0,0 +1,28 @@
+const writeCard = pokemon => {
+    const mainFlex = document.getElementById("main-container");
+    const templateCard = document.getElementById("template-card").content;
+    const clone = templateCard.cloneNode(true);
+    const fragment = document.createDocumentFragment();
+  
+    clone.querySelector(".center-img").setAttribute("src", pokemon.img);
+  
+    clone.querySelector(
+      ".center-h2"
+    ).innerHTML = `${pokemon.name} <span>${pokemon.hp}hp</span>`;
+  
+    clone.querySelector(".center-p").textContent = `${pokemon.experience} exp`;
+    clone.querySelectorAll(".footer h3")[0].textContent = pokemon.attack + "K";
+    clone.querySelectorAll(".footer h3")[1].textContent = pokemon.special + "K";
+    clone.querySelectorAll(".footer h3")[2].textContent = pokemon.defense + "K";
+  
+    fragment.appendChild(clone);
+    mainFlex.appendChild(fragment);
+  
+    const rechargeButton = document.querySelector(".center-button");
+    rechargeButton.addEventListener("click", () => location.reload());
+  };
+
+  const getRandomId = (min = 1, max = 899) =>
+  Math.floor(Math.random() * (max - min) + min);
+
+  export {writeCard, getRandomId};
---
+++ src/Scripts/app.js
@@ -0,0 +1,28 @@
+import {writeCard, getRandomId} from "./Card.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const randomId = getRandomId();
+
+  fetchData(randomId);
+});
+
+const fetchData = async (id) => {
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const dataJson = await res.json();
+
+    const pokemon = {
+      img: dataJson.sprites.other.dream_world.front_default,
+      name: dataJson.name,
+      experience: dataJson.base_experience,
+      hp: dataJson.stats[0].base_stat,
+      attack: dataJson.stats[1].base_stat,
+      defense: dataJson.stats[2].base_stat,
+      special: dataJson.stats[3].base_stat,
+    };
+
+    writeCard(pokemon);
+  } catch (error) {
+    console.log(error);
+  }
+};
